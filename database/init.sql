5af59f126c27da4e81126cf0715b28b3a190353c
Lord <157491747+Lone1eLexus@users.noreply.github.com>
2025-06-15 19:05:39 +0300

    Update init.sql

--- database/init.sql
+++ database/init.sql
@@ -1,5 +1,14 @@
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    email VARCHAR(255) NOT NULL UNIQUE CHECK (email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'),
+    password TEXT NOT NULL,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    middle_name VARCHAR(100),
+    date_of_birth DATE CHECK (date_of_birth > '1900-01-01'),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_login TIMESTAMPTZ,
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+    user_role VARCHAR(20) NOT NULL DEFAULT 'student' 
+        CHECK (user_role IN ('student', 'tutor', 'administrator'))
 );
